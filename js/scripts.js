af15cfca0426d489dde771c267291a64e63769e7
Marcin Bojarski <boj.marcin@gmail.com>
2018-03-26 17:29:01 +0200

    Add loop on scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,15 @@ $(document).ready(function() {
       type: "GET",
       dataType: 'json',
       success: function(data) {
-        $('.tweet').attr("href","https://twitter.com/intent/tweet?ref_src=twsrc%5Etfw&text="+ '"' + data.quote + '"' + " - " + data.author);
-        $('.quote').text('"' + data.quote + '"');
-        $('.author').text('--' + data.author);
-        $('.box').addClass('animated fadeInLeft').show();
+        var tweetText = '"' + data.quote + '"' + " - " + data.author;
+        if (tweetText.length < 140) {
+          $('.tweet').attr("href","https://twitter.com/intent/tweet?ref_src=twsrc%5Etfw&text="+ '"' + data.quote + '"' + " - " + data.author);
+          $('.quote').text('"' + data.quote + '"');
+          $('.author').text('--' + data.author);
+          $('.box').addClass('animated fadeInLeft').show();
+        } else {
+          data();
+        }
       },
       beforeSend: function(xhr) {
         xhr.setRequestHeader("X-Mashape-Authorization", "k0yyUeZkPpmshDLnY9BD8bFhgm5mp15QiNXjsncA14OFYnCyOH")
