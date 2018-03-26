6599e43422a3dab4ba6d4e093afb7a57c0ec97f2
Marcin Bojarski <boj.marcin@gmail.com>
2018-03-26 17:20:50 +0200

    Fix scripts.js and index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,22 @@
 $(document).ready(function() {
-
-function quote(){
+  $('.box').hide();
+  $('#generator').click(function() {
+    $('.box').hide();
+    $('.quote').text("");
+    $('.author').text("");
     $.ajax({
-        url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
-        type: 'post',
-        data: {
-
-        },
-        headers: {
-            'X-Mashape-Key': '1Xuhcdh7NsmshWXJ9mNNa3ChMDpvp1fIoBNjsnLnPJiwUxrMGO'
-        },
-        dataType: 'json',
-        success: function(data) {
-            //$('body').fadeIn(50);
-            $('.real-quote>span>p').text('"' + data.quote + '"');
-            $('.author>span').text(data.author);
-            $('.author>small').text("Category: " + data.category);
-            $('#twitter').attr("href","https://twitter.com/intent/tweet?ref_src=twsrc%5Etfw&text="+ '"' + data.quote + '"' + " - " + data.author +"&tw_p=tweetbutton&url=http%3A%2F%2Fcodepen.io%2Fgexo%2Fpen%2FvOPxdg" );
-        }
-    });
-}
-
-    $('#btn').click(function(e) {
-      e.preventDefault();
-       quote();
-
-
+      url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
+      type: "GET",
+      dataType: 'json',
+      success: function(data) {
+        $('.tweet').attr("href","https://twitter.com/intent/tweet?ref_src=twsrc%5Etfw&text="+ '"' + data.quote + '"' + " - " + data.author + "&tw_p=tweetbutton&url=http%3A%2F%2Fcodepen.io%2Fgexo%2Fpen%2FvOPxdg" );
+        $('.quote').text('"' + data.quote + '"');
+        $('.author').text('--' + data.author);
+        $('.box').addClass('animated fadeInLeft').show();
+      },
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader("X-Mashape-Authorization", "k0yyUeZkPpmshDLnY9BD8bFhgm5mp15QiNXjsncA14OFYnCyOH")
+      }
+    })
+  })
 });
-  quote();
-  });
